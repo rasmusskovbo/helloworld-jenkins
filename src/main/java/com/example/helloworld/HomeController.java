4d0532ac0fe269c5a8dbc2fe2c0748b739d8f763
Rasmus Skovbo <63857731+rasmusskovbo@users.noreply.github.com>
2021-02-12 13:50:24 +0100

    Update HomeController.java

--- src/main/java/com/example/helloworld/HomeController.java
+++ src/main/java/com/example/helloworld/HomeController.java
@@ -9,7 +9,7 @@ import java.sql.Connection;
 @Controller
 public class HomeController {
 
-    public String name= "anna";
+    public String name= "everything";
 
     @GetMapping("/")
     public String home(){
